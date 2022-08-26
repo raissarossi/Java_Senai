d47b517901eba52eaf6a9db43eb3bc9e814f8771
raissarossi <109825210+raissarossi@users.noreply.github.com>
2022-08-26 16:59:46 -0300

    Update BhaskaraSerto.java

--- Bhaskara/BhaskaraSerto.java
+++ Bhaskara/BhaskaraSerto.java
@@ -1,14 +1,18 @@
 public class BhaskaraSerto {
     public static int VerificarSinal (String num){
         int sinal = -1;
-        int mais = num.indexOf("+");
-        int menos = num.indexOf("-");
+        int mais = num.indexOf("+",1);
+        int menos = num.indexOf("-",1);
         System.out.println("valordemenos"+menos);
-        if          ((mais < menos)&&(mais != 0)){
-            sinal = menos;
-        }else if    ((menos < mais)&&(menos != 0)){
-            sinal = mais;
+        if (mais == -1){
+            mais = menos+1;
         }
+        if (menos == -1){
+            menos = mais+1;
+        }
+
+        sinal = (mais < menos?mais:menos);
+
         return sinal;
     }
 
@@ -16,13 +20,15 @@ public class BhaskaraSerto {
     public static void main(String[] args) {
         
         System.out.println("Ola bom dia, para funcionar corretamente digite a equacao reduzida\n");
-        String num = "-1x²+2x+3";
+        String num = "1x²+2x+3";
         num = num.replaceAll(",", ".");
         if (num.indexOf("-")==0){}//do nothing
         else if (num.indexOf("+")==0){}//do nothing
         else{
             num = "+" + num;
         }
+        num = num + "+";
+        num = num + "=";
         num = num.toUpperCase();
         num = num.replace("²", "^2");
 
@@ -33,5 +39,18 @@ public class BhaskaraSerto {
 
         System.out.println("k"+sinal);
 
+        String akiA = num.substring(0, sinal);
+        System.out.println(akiA);
+                //RETIRAR O VALOR DE AKIA DE NUM
+
+        sinal = VerificarSinal(num);
+        String akiB = num.substring(0, sinal);
+        System.out.println(akiB);
+                //RETIRAR O VALOR DE AKIA DE NUM
+
+        sinal = VerificarSinal(num);
+        String akiC = num.substring(0, sinal);
+        System.out.println(akiC);
     }
 }
+
